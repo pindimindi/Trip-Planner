f7cf61fe7d948919f347f434191555df3cd0e0d9
Kevin Gislason <kevingislason@gmail.com>
2018-08-07 15:55:30 -0500

    please work

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-const mapboxgl = require("mapbox-gl");
+const mapboxgl = require('mapbox-gl');
 
-mapboxgl.accessToken = "pk.eyJ1IjoiaWRvbnR3YW50dG8iLCJhIjoiY2prazQyMzZ2NjI3bzNxbWxpMWt4anA1MCJ9.XELVWmy1OWCYj-_RKi9iKA";
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiaWRvbnR3YW50dG8iLCJhIjoiY2prazQyMzZ2NjI3bzNxbWxpMWt4anA1MCJ9.XELVWmy1OWCYj-_RKi9iKA';
 
 const map = new mapboxgl.Map({
-  container: "map",
+  container: 'map',
   center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
   zoom: 12, // starting zoom
-  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+  style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
 });
+
+const markerDomEl = document.createElement('div');
+markerDomEl.style.width = '32px';
+markerDomEl.style.height = '39px';
+markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/WbMOfMl.png)';
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009151, 40.705086]).addTo(map);
